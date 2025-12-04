aa233a26ae9a9f53518fff0b0868e111f833b821
GitHub Action <action@github.com>
2025-12-04 20:07:38 +0000

    chore: auto-generate Python classes from schema changes
    Auto-generated Python classes from LinkML schema updates.
    
    **Auto-generated by GitHub Actions workflow**
    [skip ci]

--- modules/Sequencing/src/htan_sequencing/datamodel/sequencing.py
+++ modules/Sequencing/src/htan_sequencing/datamodel/sequencing.py
@@ -1,5 +1,5 @@
 # Auto generated from sequencing.yaml by pythongen.py version: 0.0.1
-# Generation date: 2025-12-04T19:54:18
+# Generation date: 2025-12-04T20:06:25
 # Schema: Sequencing
 #
 # id: https://w3id.org/htan/sequencing
