81845d7d5dd61e8c658d937c37bd3db58ec090ed
aditigopalan <63365451+aditigopalan@users.noreply.github.com>
2026-01-07 15:52:43 -0500

    Add custom CSS to increase documentation content width

--- conf.py
+++ conf.py
@@ -13,6 +13,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static', 'modules/CoreFile']
+html_css_files = ['custom.css']
 
 source_suffix = ['.md', '.rst']
 myst_enable_extensions = ['colon_fence', 'deflist']
