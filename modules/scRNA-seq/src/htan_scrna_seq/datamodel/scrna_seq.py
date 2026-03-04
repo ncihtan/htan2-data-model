fdeec52163bd452470909fcbad5367c7d1489dd2
GitHub Action <action@github.com>
2026-03-04 20:08:22 +0000

    chore: auto-generate Python classes from schema changes
    Auto-generated Python classes from LinkML schema updates.
    
    **Auto-generated by GitHub Actions workflow**
    [skip ci]

--- modules/scRNA-seq/src/htan_scrna_seq/datamodel/scrna_seq.py
+++ modules/scRNA-seq/src/htan_scrna_seq/datamodel/scrna_seq.py
@@ -1,5 +1,5 @@
 # Auto generated from scrna_seq.yaml by pythongen.py version: 0.0.1
-# Generation date: 2026-03-04T20:01:31
+# Generation date: 2026-03-04T20:08:16
 # Schema: scRNA-seq
 #
 # id: https://w3id.org/htan/scrna_seq
