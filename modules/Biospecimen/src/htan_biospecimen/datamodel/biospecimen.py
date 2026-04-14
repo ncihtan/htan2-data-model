e25e81fd56b55fc53048e3588ccf6bf064eaed0e
GitHub Action <action@github.com>
2026-04-14 18:12:27 +0000

    chore: auto-generate Python classes from schema changes
    Auto-generated Python classes from LinkML schema updates.
    
    **Auto-generated by GitHub Actions workflow**
    [skip ci]

--- modules/Biospecimen/src/htan_biospecimen/datamodel/biospecimen.py
+++ modules/Biospecimen/src/htan_biospecimen/datamodel/biospecimen.py
@@ -1,5 +1,5 @@
 # Auto generated from biospecimen.yaml by pythongen.py version: 0.0.1
-# Generation date: 2026-04-14T17:41:00
+# Generation date: 2026-04-14T18:12:04
 # Schema: Biospecimen
 #
 # id: https://w3id.org/htan/biospecimen
