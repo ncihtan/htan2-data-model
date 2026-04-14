e7d98404289175b47916a854eb56caee89732928
GitHub Action <action@github.com>
2026-04-14 19:56:11 +0000

    chore: auto-generate Python classes from schema changes
    Auto-generated Python classes from LinkML schema updates.
    
    **Auto-generated by GitHub Actions workflow**
    [skip ci]

--- modules/DigitalPathology/src/htan_digitalpathology/datamodel/digital_pathology.py
+++ modules/DigitalPathology/src/htan_digitalpathology/datamodel/digital_pathology.py
@@ -1,5 +1,5 @@
 # Auto generated from digital_pathology.yaml by pythongen.py version: 0.0.1
-# Generation date: 2026-04-14T19:09:20
+# Generation date: 2026-04-14T19:56:06
 # Schema: DigitalPathology
 #
 # id: https://w3id.org/htan/digital_pathology
