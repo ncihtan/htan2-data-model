f686fb3ceb136cb4c698f1b7be45ef663252ddf6
GitHub Action <action@github.com>
2026-04-30 17:14:51 +0000

    chore: auto-generate Python classes from schema changes
    Auto-generated Python classes from LinkML schema updates.
    
    **Auto-generated by GitHub Actions workflow**
    [skip ci]

--- modules/SpatialOmics/src/htan_spatial/datamodel/spatial.py
+++ modules/SpatialOmics/src/htan_spatial/datamodel/spatial.py
@@ -1,5 +1,5 @@
 # Auto generated from spatial.yaml by pythongen.py version: 0.0.1
-# Generation date: 2026-04-30T16:46:47
+# Generation date: 2026-04-30T17:14:50
 # Schema: SpatialOmics
 #
 # id: https://w3id.org/htan/spatial
--- modules/scRNA-seq/src/htan_scrna_seq/datamodel/scrna_seq.py
+++ modules/scRNA-seq/src/htan_scrna_seq/datamodel/scrna_seq.py
@@ -1,5 +1,5 @@
 # Auto generated from scrna_seq.yaml by pythongen.py version: 0.0.1
-# Generation date: 2026-04-30T16:46:42
+# Generation date: 2026-04-30T17:14:45
 # Schema: scRNA-seq
 #
 # id: https://w3id.org/htan/scrna_seq
